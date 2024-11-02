b7740635d4e4c50ec456d02444b3a05480c6f292
Dennis <dennisdiepolder@outlook.com>
2024-11-02 19:49:11 +0100

    smooth-scrolling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import SmoothScrolling from "@/utils/SmoothScrolling";
+
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,8 +27,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SmoothScrolling>
+
+        
         <Navbar />
         {children}
+        </SmoothScrolling>
       </body>
     </html>
   );
